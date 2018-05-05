d0a4b05059af7a3beda8359c65d87fc70b22c8e3
David Baumgartner <2er0@dbaumi.at>
2018-05-05 22:35:06 +0200

    fixed issue with docker entrypoint and params for nodejs

--- web/settings-user.js
+++ web/settings-user.js
@@ -1,7 +1,7 @@
 module.exports = {
 	default: {  // Those can be overhidden in the browser
-		host: 'core',  // quasselcore host
-		port: 4242,  // quasselcore port
+		host: 'home.dbaumi.at',  // quasselcore host
+		port: 44242,  // quasselcore port
 		initialBacklogLimit: 20,  // Amount of backlogs to fetch per buffer on connection
 		backlogLimit: 100,  // Amount of backlogs to fetch per buffer after first retrieval
 		securecore: true,  // Connect to the core using SSL
@@ -13,9 +13,9 @@ module.exports = {
 	},
 	webserver: {
 		socket: false,  // Tells the webserver to listen for connections on a local socket. This should be a path. Can be overhidden by '--socket' argument
-		listen: null,  // Address on which to listen for connection, defaults to listening on all available IPs. Can be overhidden by '--listen' argument
-		port: null,  // Port on which to listen for connection, defaults to 64080 for http mode, 64443 for https. Can be overhidden by '--port' argument
-		mode: null  // can be 'http' or 'https', defaults to 'https'. Can be overhidden by '--mode' argument
+		listen: 0.0.0.0,  // Address on which to listen for connection, defaults to listening on all available IPs. Can be overhidden by '--listen' argument
+		port: 64080,  // Port on which to listen for connection, defaults to 64080 for http mode, 64443 for https. Can be overhidden by '--port' argument
+		mode: http  // can be 'http' or 'https', defaults to 'https'. Can be overhidden by '--mode' argument
 	},
 	themes: ['default', 'darksolarized'],  // Available themes
 	forcedefault: true,  // Will force default host and port to be used if true, and will hide the corresponding fields in the UI.
